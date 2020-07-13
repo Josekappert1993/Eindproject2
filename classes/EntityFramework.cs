8fb4bdac5be18aeb2bf42e2ea9af48e761778b1e
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-07-13 13:37:34 +0200

    Database basis

--- classes/EntityFramework.cs
+++ classes/EntityFramework.cs
@@ -69,12 +69,77 @@ namespace Eindproject2
         public Subtiltle subtitle_id { set; get; }
     }
 
+    //package table
+    public class Package
+    {
+        [Key]
+        public int id { get; set; }
+        public float price { get; set; }
+        public int length { get; set; }
+    }
+
+    //preference table
+    public class Preference
+    {
+        [Key]
+        public int user_id { get; set; }
+        public Genre genre_id { get; set; }
+        public Age_Advice age_Advice { get; set; }
+        public Profile profile_id { get; set; }
+        public Account profile_account_id { get; set; }
+        public Watchlist profile_watchlist_id { get; set; }
+    }
+
+    //Profile table
+    public class Profile
+    {
+        [Key]
+        public int id { get; set; }
+        public Account account_id { get; set; }
+        public string name { get; set; }
+        public string profile_picture { get; set; }
+        public int age { get; set; }
+        public string user_language { get; set; }
+        public int times_pasword_reset { get; set; }
+    }
+
+    //seasons table
+    public class Season
+    {
+        [Key]
+        public int id { get; set; }
+        public string season_name { get; set; }
+        public Serie serie_id { get; set; }
+        public string episode_name { get; set; }
+        public int episode_length { get; set; }
+        public string file { get; set; }
+    }
+
     //serie table
     public class Serie
     {
         [Key]
         public int id { get; set;}
+        public string name { get; set; }
+    }
 
+    //subscription table
+    public class Subscription
+    {
+        [Key]
+        public int id { get; set; }
+        public Package package_id { get; set; }
+        public string subscription_name { get; set; }
+        public DateTime start_date { get; set; }
+        public Account account_id { get; set; }
+    }
+
+    //subscription_has_account table
+    public class Subscription_has_account
+    {
+        public Subscription subscription_id { get; set; }
+        public Account account_id { get; set; }
+        public Profile profile_id { get; set; }
     }
 
     //subtitle table
@@ -82,8 +147,30 @@ namespace Eindproject2
     {
         [Key]
         public int id { get; set; }
+        public string subtitle_language { get; set; }
+        public string subtitle_fila { get; set; }
     }
 
+    //to_watchlist table
+    public class To_watchlist
+    {
+        [Key]
+        public int id { get; set; }
+        public Media media_id { get; set; }
+        public Profile profile_id { get; set; }
+    }
+
+    //watchlist table
+    public class Watchlist
+    {
+        [Key]
+        public int id { get; set; }
+        public int paused_time { get; set; }
+        public Media media { get; set; }
+        public Profile profile { get; set; }
+        public Boolean finished { get; set; }
+    }
+    //Database setup
     public class NetflixContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
@@ -91,5 +178,15 @@ namespace Eindproject2
         public DbSet<Film> Films { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Media> Medias { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Preference> Preferences { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Serie> Series { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<Subscription_has_account> subscription_Has_Accounts { get; set; }
+        public DbSet<Subtiltle> Subtiltles { get; set; }
+        public DbSet<To_watchlist> To_Watchlists { get; set; }
+        public DbSet<Watchlist> Watchlists { get; set; }
     }
 }
